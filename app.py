a9d0831e455f41556f679dc33e82e4466239477b
DanielZh <haveacar.zhovnir@gmail.com>
2023-01-29 21:55:29 +0200

    removed db

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 
+
+# config flask
 app = Flask(__name__)
 
 
+# home page
 @app.route('/')
 @app.route('/home')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run()
+

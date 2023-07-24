336b6568dedaa918ba64d69c5a6be9f669ac5036
DanielZh <haveacar.zhovnir@gmail.com>
2023-07-24 14:03:31 +0300

    added certification

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from flask import Flask, render_template, url_for
 # config flask
 app = Flask(__name__)
 
+images = images = [
+        "img/cer1.jpg",
+        "img/cer2.jpg",
+        "img/certificate_python_institute.png",
+        "img/spectr.jpg",
+    ]
+
 # home page
 @app.route('/')
 @app.route('/home')
 def main_page():
-    return render_template('index.html')
+    return render_template('index.html', images=images)
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5007)
+    app.run(debug=False, port=5000)
